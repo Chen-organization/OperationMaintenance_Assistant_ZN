28651d370fde6e8dd55c37b7996c7b3742bc3d59
Apple <apple@ACA803FA.ipt.aol.com>
2018-12-28 19:39:53 +0800

    fmdb

---
+++ OperationMaintenance_Assistant_ZN/OperationMaintenance_Assistant_ZN/Utils/fmdbManager/SqliteManager.h
@@ -0,0 +1,26 @@
+//
+//  SqliteManager.h
+//  YunDi_Student
+//
+//  Created by apple on 2017/6/7.
+//  Copyright © 2017年 Fengyun Diyin Technologies (Beijing) Co., Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef void (^completeBlock) (BOOL isSure, NSArray *result);
+
+@interface SqliteManager : NSObject
+
++ (instancetype)shareManager;
+
+- (void)setupAddressSqlite;
+- (void)insertNewHomeVideoListWithArray:(NSArray *)arr;
+- (NSArray *)cacheNewHomeVideoList;//:(IWRequestParameters *)parameters
+
+- (void)setupBannerSqlite;
+-(void)insertNewHomeBannerListWithArray:(NSArray *)arr;
+- (NSArray *)cacheNewHomeBannerList;
+
+
+@end
