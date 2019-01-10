1b2d39366c0146311009478bd0795ef919369f5f
OlegChen <q729898154@163.com>
2019-01-11 01:31:31 +0800

    orderDetail

---
+++ OperationMaintenance_Assistant_ZN/OperationMaintenance_Assistant_ZN/Module/Home/orderDetail/orderDetailVC.swift
@@ -0,0 +1,30 @@
+//
+//  orderDetailVC.swift
+//  OperationMaintenance_Assistant_ZN
+//
+//  Created by Chen on 2019/1/10.
+//  Copyright © 2019年 Chen. All rights reserved.
+//
+
+import UIKit
+
+class orderDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
